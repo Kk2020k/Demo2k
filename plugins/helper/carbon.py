4f6e458dc7715d55afdc3bcef43ba49d04ec6720
Nobi Developer <125588667+NobiDeveloper@users.noreply.github.com>
2023-12-30 07:17:28 +0530

    Update carbon.py

--- plugins/helper/carbon.py
+++ plugins/helper/carbon.py
@@ -43,7 +43,7 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**ᴛʜɪs ᴘɪᴄ ɪs ɴɪᴄᴇ ᴏɴᴇ\nʙʏ @BotszList**",
+        caption="**ᴛʜɪs ᴘɪᴄ ɪs ɴɪᴄᴇ ᴏɴᴇ\nʙʏ @MovieVillaYT**",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=S_GROUP)]]),                   
     )
     await m.delete()
