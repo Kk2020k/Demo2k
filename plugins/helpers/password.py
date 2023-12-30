972fad7726a5cccdea7cd4d489569e96738e48dd
Nobi Developer <125588667+NobiDeveloper@users.noreply.github.com>
2023-12-30 07:23:18 +0530

    Update password.py

--- plugins/helpers/password.py
+++ plugins/helpers/password.py
@@ -12,5 +12,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ 🔐', url='https://telegram.me/BotszSupport')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ 🔐', url='https://telegram.me/NobiDeveloperSupport')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
