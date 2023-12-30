4832b88ea711be22c7a27859d58061d7542ebc7d
Nobi Developer <125588667+NobiDeveloper@users.noreply.github.com>
2023-12-30 07:25:54 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [ᴄʜᴀɴɴᴇʟ](https://youtube.com/@MovieVillaYT)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [ᴍᴏᴠɪᴇ ᴠɪʟʟᴀ](https://telegram.me/MovieVillaYT)**'
+        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [ᴄʜᴀɴɴᴇʟ](https://youtube.com/@NobiDeveloper)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [ᴍᴏᴠɪᴇ ᴠɪʟʟᴀ](https://telegram.me/MovieVillaYT)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -140,7 +140,7 @@ async def vsong(client, message: Message):
     capy = f"""
 **𝚃𝙸𝚃𝙻𝙴 :** [{thum}]({mo})
 **𝚁𝙴𝚀𝚄𝙴𝚂𝚃𝙴𝙳 𝙱𝚈 :** {message.from_user.mention}
-**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 :** [ᴍᴏᴠɪᴇ ᴠɪʟʟᴀ](https://youtube.com/@MovieVillaYT)
+**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 :** [ᴍᴏᴠɪᴇ ᴠɪʟʟᴀ](https://telegram.me/MovieVillaYT)
 """
     await client.send_video(
         message.chat.id,
