b9f8c5621af1bb849a58dfa3fe17adc950326422
Nobi Developer <125588667+NobiDeveloper@users.noreply.github.com>
2024-01-21 17:29:45 +0530

    Update openai.py

--- plugins/helpers/openai.py
+++ plugins/helpers/openai.py
@@ -1,15 +1,17 @@
-from info import S_GROUP, OPENAI_API
+from info import SUPPORT_GROUP, S_GROUP, OPENAI_API
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-import openai
+from openai import OpenAI
 
-openai.api_key = OPENAI_API
+ai_client = OpenAI(api_key=OPENAI_API)
 
 @Client.on_message(filters.command("openai"))
-async def openai(client, message):
-    if message.chat.id != S_GROUP:
+async def ask_question(client, message):
+    if len(OPENAI_API) == 0:
+        return await message.reply("OPENAI_API is empty")
+    if message.chat.id != SUPPORT_GROUP:
         btn = [[
-            InlineKeyboardButton('Support Group', url="https://telegram.me/NobiDeveloperSupport")
+            InlineKeyboardButton('Support Group', url=S_GROUP)
         ]]
         return await message.reply("This command only working in support group.", reply_markup=InlineKeyboardMarkup(btn))
     try:
@@ -18,11 +20,12 @@ async def openai(client, message):
         return await message.reply_text("Command Incomplete!\nUsage: /openai your_question")
     msg = await message.reply("Searching...")
     try:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=text,
-            max_tokens=2000
+        response = ai_client.chat.completions.create(
+            messages=[
+                {"role": "user", "content": text}
+            ],
+            model="gpt-3.5-turbo"
         )
-        await msg.edit(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response.choices[0].text}</code>")
+        await msg.edit(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response.choices[0].message.content}</code>")
     except Exception as e:
         await msg.edit(f'Error - <code>{e}</code>')
