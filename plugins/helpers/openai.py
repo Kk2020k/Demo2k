ccea2b3fd3440180cd5a075109c30d7a3d9350f2
Nobi Developer <125588667+NobiDeveloper@users.noreply.github.com>
2024-02-16 20:14:27 +0530

    Update openai.py

--- plugins/helpers/openai.py
+++ plugins/helpers/openai.py
@@ -3,15 +3,13 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import openai
 
-ai_client = OpenAI(api_key=OPENAI_API)
+openai.api_key = OPENAI_API
 
 @Client.on_message(filters.command("openai"))
 async def ask_question(client, message):
-    if len(OPENAI_API) == 0:
-        return await message.reply("OPENAI_API is empty")
     if message.chat.id != S_GROUP:
         btn = [[
-            InlineKeyboardButton('Support Group', url=S_GROUP)
+            InlineKeyboardButton('Support Group', url="https://telegram.me/NobiDeveloperSupport")
         ]]
         return await message.reply("This command only working in support group.", reply_markup=InlineKeyboardMarkup(btn))
     try:
@@ -20,12 +18,11 @@ async def ask_question(client, message):
         return await message.reply_text("Command Incomplete!\nUsage: /openai your_question")
     msg = await message.reply("Searching...")
     try:
-        response = ai_client.chat.completions.create(
-            messages=[
-                {"role": "user", "content": text}
-            ],
-            model="gpt-3.5-turbo"
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=text,
+            max_tokens=2000
         )
-        await msg.edit(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response.choices[0].message.content}</code>")
+        await msg.edit(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response.choices[0].text}</code>")
     except Exception as e:
         await msg.edit(f'Error - <code>{e}</code>')
