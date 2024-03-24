93a9caf4610ecad6fd4c96acb29f16ffe6d92e69
monitg <142651228+monitg@users.noreply.github.com>
2024-03-24 09:52:14 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [ᴄʜᴀɴɴᴇʟ](https://youtube.com/@NobiDeveloper)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [ᴍᴏᴠɪᴇ ᴠɪʟʟᴀ](https://telegram.me/MovieVillaYT)**'
+        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [ᴄʜᴀɴɴᴇʟ](https://youtube.com/@MoviesdudeZ)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [Team_MDL](https://telegram.me/Team_MDL)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
