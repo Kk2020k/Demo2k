ee7d909e64d419afa3d9beacb6e325425688a376
monitg <142651228+monitg@users.noreply.github.com>
2024-03-24 11:07:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/d2f6fe4c981c91b97ca66.jpg https://telegra.ph/file/9acca333738f41eba89f8.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/9acca333738f41eba89f8.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/9acca333738f41eba89f8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/9acca333738f41eba89f8.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/9acca333738f41eba89f8.jpg")
