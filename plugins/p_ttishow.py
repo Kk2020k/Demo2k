a9ba1aec3ecbc791ca8b2105f65dd837d9c42ade
monitg <142651228+monitg@users.noreply.github.com>
2024-03-24 10:11:16 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -23,7 +23,7 @@ async def save_group(bot, message):
         if message.chat.id in temp.BANNED_CHATS:
             # Inspired from a boat of a banana tree
             buttons = [[
-                InlineKeyboardButton('Support', url='https://telegram.me/NobiDeveloperSupport')
+                InlineKeyboardButton('Support', url='https://telegram.me/Team_MDL_Admin_Bot')
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             k = await message.reply(
@@ -38,8 +38,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-                    InlineKeyboardButton('🥷 ʜᴇʟᴘ 🥷', url='https://telegram.me/NobiDeveloperSupport'),
-                    InlineKeyboardButton('♻️ ᴜᴘᴅᴀᴛᴇꜱ ♻️', url='https://telegram.me/NobiDeveloper')
+                    InlineKeyboardButton('🥷 ʜᴇʟᴘ 🥷', url='https://telegram.me/Team_MDL_Admin_Bot'),
+                    InlineKeyboardButton('♻️ ᴜᴘᴅᴀᴛᴇꜱ ♻️', url='https://telegram.me/Team_MDL')
                  ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -59,7 +59,7 @@ async def save_group(bot, message):
                                                  caption=(script.MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
-                                                                           InlineKeyboardButton('ʀᴇᴀᴅ ɢʀᴏᴜᴘ ʀᴜʟᴇs 📌', url='https://telegra.ph/Movie-Villa-01-11')
+                                                                           InlineKeyboardButton('ʀᴇᴀᴅ ɢʀᴏᴜᴘ ʀᴜʟᴇs 📌', url='https://t.me/Team_MDL')
                                                                         ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
@@ -80,7 +80,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url='https://telegram.me/NobiDeveloperSupport')
+            InlineKeyboardButton('Support', url='https://telegram.me/Team_MDL_Admin_Bot')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
@@ -119,7 +119,7 @@ async def disable_chat(bot, message):
     await message.reply('Chat Successfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url='https://telegram.me/NobiDeveloperSupport')
+            InlineKeyboardButton('Support', url='https://telegram.me/Team_MDL_Admin_Bot')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
