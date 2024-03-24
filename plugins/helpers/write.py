99fc5c5d2c0644dd6365305306c90e23518bac9d
monitg <142651228+monitg@users.noreply.github.com>
2024-03-24 09:53:23 +0530

    Update write.py

--- plugins/helpers/write.py
+++ plugins/helpers/write.py
@@ -15,7 +15,7 @@ async def handwrite(_, message: Message):
         req = requests.get(API).url
         await message.reply_photo(
             photo=req,
-            caption="""<b>✍  ʙʏ  -  <a href=https://telegram.me/NobiDeveloperr>@NobiDeveloperr</a></b>""",
+            caption="""<b>✍  ʙʏ  -  <a href=https://telegram.me/Team_MDL>@Team_MDL</a></b>""",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("📑    ᴛᴇʟᴇɢʀᴀᴘʜ  ʟɪɴᴋ    📑", url=f"{req}")]]
             ),
